2b164ad4417f22ace91fa18a03277e644c3ec4c2
Akshay Wakle <108985906+akshay-webdev@users.noreply.github.com>
2023-11-27 09:27:10 +0530

    ui changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import { useState } from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-
+import Home from "./components/Home";
+import About from "./components/About";
+import Project from "./components/Project";
+import Contact from "./components/Contact"
 function App() {
   const [getcolor, setColor] = useState(false);
 
   const changeBackground = () => {
     setColor((prev) => !prev);
   };
-  
+
   const backgroundColor = {
-    backgroundColor: getcolor ? '#fff' : '#0d2438',
+    backgroundColor: getcolor ? "#fff" : "#0d2438",
   };
-  const changeButtonImage=getcolor? 'bulb-default.svg':'bulb.svg';
+  const changeButtonImage = getcolor ? "bulb-default.svg" : "bulb.svg";
 
   return (
-    <div className="App" style={backgroundColor}>
-      <Navbar changeBackground={changeBackground} changeButtonImage={changeButtonImage}/>
+    <div style={backgroundColor}>
+   <Router>
+      <div className="App container">
+      <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/About" element={<About />} />
+          <Route path="/Project" element={<Project />} />
+          <Route path="/Contact" element={<Contact/>}/>
+        </Routes>
+        <Navbar
+          changeBackground={changeBackground}
+          changeButtonImage={changeButtonImage}
+        />
+        <Home/>
+        <About/>
+        <Project/>
+        <Contact/>
+        
+        
+       
+      </div>
+    </Router>
     </div>
+ 
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+   <>
+    <div className='row d-flex align-items-center'>
+      <div className='col-md-5'>
+          <h5>Hi, Iam Akshay</h5>
+          <h6>A Front-End Developer</h6>
+          <button className='download-btn'>Download CV</button>
+      </div>
+      <div className='col-md-7'>
+          <img src='./h5.svg'/>
+      </div>
+
+    </div>
+
+
+   </>
   )
 }
 
