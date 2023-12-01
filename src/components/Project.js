0bec4bfed68eddade913b9112e52b4cf0f6b7114
Akshay Wakle <108985906+akshay-webdev@users.noreply.github.com>
2023-12-01 14:07:30 +0530

    ui changes

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,21 +6,28 @@ const Project = ({ Data }) => {
 
   return (
     <>
-    <div className="row" style={{gap:'39px',width:'100%'}}>
-      {getData.map((data, i) => (
-          <div className="card col-3" style={{width:'31%'}} key={i}>
-              <img src={data.image} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">{data.project}</h5>
-              <h6>{data.type}</h6>
-            </div>
-            <div className="card-footer">
-              <button className="btn btn-primary me-2">Source Code</button>
-              <button className="btn btn-secondary ms-2">Try it Live</button>
-            </div>
-          </div>
-        ))}
+    <div className="project-section">
+    <h4 className="sec-title">Projects portfolio</h4>
+
+<input placeholder="Search Projects" className="searchbar"/>
+
+<div className="row" style={{gap:'39px',width:'100%'}}>
+  {getData.map((data, i) => (
+      <div className="card col-3" style={{width:'31%'}} key={i}>
+          <img src={data.image} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{data.project}</h5>
+          <h6>{data.type}</h6>
+        </div>
+        <div className="card-footer">
+          <button className="btn btn-primary me-2">Source Code</button>
+          <button className="btn btn-secondary ms-2">Try it Live</button>
+        </div>
       </div>
+    ))}
+  </div>
+    </div>
+
     </>
   );
 };
